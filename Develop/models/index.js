82bedde1da4de279796dc0a030aa1df2382b0fe8
Jessica Luff <78363096+JessLuff@users.noreply.github.com>
2021-05-31 22:52:38 +1000

    Files amended

---
+++ Develop/models/index.js
@@ -0,0 +1,41 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    mode: ProductTag,
+    unique: false
+  },
+//as: ""
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    mode: ProductTag,
+    unique: false
+  },
+//as: ""
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
